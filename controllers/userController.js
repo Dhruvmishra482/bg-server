fe07f53fbb317f02f2692865e3c30b1aa64ac0f1
dhruvmishra482 <dhruvmishra3829@gmail.com>
2025-07-27 10:32:42 +0530

    Updated auth middleware to fix clerkId decoding and error handling

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,63 +2,10 @@ import { Webhook } from "svix";
 import userModel from "../models/userModel.js";
 import Razorpay from 'razorpay'
 import transactionModel from "../models/transactionModel.js";
-// API controller Function to manage clerk user with database
-// http://localhost:4000/api/user/webhooks
-
-// const clerkWebhooks = async (req, res) => {
-// 	try {
-// 		// Create a svix instance with clerk webhook secret
-// 		const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-
-// 		await whook.verify(JSON.stringify(req.body), {
-// 			"svix-id": req.headers["svix-id"],
-// 			"svix-timestamp": req.headers["timestamp"],
-// 			"svix-signature": req.headers["svix-signature"],
-// 		});
-
-// 		const { data, type } = req.body;
-
-// 		switch (type) {
-// 			case "user.created": {
-// 				const userData = {
-// 					clerkId: data.id,
-// 					email: data.email_addresses[0].email_address,
-// 					firstName: data.first_name,
-// 					lastName: data.last_name,
-// 					photo: data.image_url,
-// 				};
-// 				await userModel.create(userData);
-// 				res.json({ success: true, message: "User Created" });
-
-// 				break;
-// 			}
-// 			case "user.updated": {
-// 				const userData = {
-// 					email: data.email_addresses[0].email_address,
-// 					firstName: data.first_name,
-// 					lastName: data.last_name,
-// 					photo: data.image_url,
-// 				};
-// 				await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
-// 				res.json({});
-// 				break;
-// 			}
-// 			case "user.deleted": {
-// 				await userModel.findOneAndDelete({ clerkId: data.id });
-// 				res.json({});
-// 				break;
-// 			}
-
-// 			default:
-// 				break;
-// 		}
-// 	} catch (error) {
-// 		console.log(error);
-// 		res.json({ success: false, message: error.message });
-// 	}
-// };
 
 const clerkWebhooks = async (req, res) => {
+    console.log("Webhook Triggered", type, data);
+
     try {
         // Create a svix instance with clerk webhook secret
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
@@ -111,16 +58,22 @@ const clerkWebhooks = async (req, res) => {
 
 // API controller functopn to get user available credits data
 const userCredits = async (req, res) => {
-	try {
-		const { clerkId } = req.body;
-		const userData = await userModel.findOne({ clerkId });
+  try {
+    const { clerkId } = req.body;
 
-		res.json({ success: true, credits: userData.creditBalance });
-	} catch (error) {
-		console.log(error);
-		res.json({ success: false, message: error.message });
-	}
+    const userData = await userModel.findOne({ clerkId });
+
+    if (!userData) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    res.json({ success: true, credits: userData.creditBalance });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
 };
+
 
 // Gateway initialize 
 // const razorpayInstance = ({
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,33 @@
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 // Middleware function to decode jwt token to get clerkId
-const authUser = async (req,res, next)=>{
-    try {
-       const {token} = req.headers 
+const authUser = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
 
-       if(!token){
-        res.status(401).json({success:false, message:"Not Authorized login again!"})
-       }
-
-       const token_decode = jwt.decode(token)
-       console.log(token_decode);
-       
-       //    req.body.clerkId = 'user_2nqyPylJ84G1Xzw8XlwBDxkALeJ'
-       req.body.clerkId = token_decode.clerkId
-       next()
-    } catch (error) {
-        console.log(error)
-        res.status(403).json({success:false,message:error.message})
+    // Token required
+    if (!token) {
+      return res.status(401).json({ success: false, message: "Not Authorized. Login again!" });
     }
-}
+
+    // Decode token (no secret because it's a session token from Clerk)
+    const token_decode = jwt.decode(token);
+
+    // Check if token_decode is valid and has clerkId
+    if (!token_decode || !token_decode.clerkId) {
+      return res.status(403).json({ success: false, message: "Invalid token. ClerkId not found" });
+    }
+
+    // Attach clerkId to request
+    req.body.clerkId = token_decode.clerkId;
+
+    // Continue
+    next();
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 export default authUser;
